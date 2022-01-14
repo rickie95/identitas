84e8189cb61039880b14ce35522994238addba31
rickie95 <rickie95>
2022-01-15 00:43:33 +0100

    initial commit

---
+++ src/main/java/it/riccardomalavolti/identitas/IdentitasApplication.java
@@ -0,0 +1,13 @@
+package it.riccardomalavolti.identitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/riccardomalavolti/identitas/IdentitasApplicationTests.java
@@ -0,0 +1,13 @@
+package it.riccardomalavolti.identitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
